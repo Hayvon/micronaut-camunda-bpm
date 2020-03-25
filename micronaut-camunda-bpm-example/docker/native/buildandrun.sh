e322e22dbf627d01ff11be3313da3ad93e25c601
Aleksandr Arshavskiy <aleksandr.arshavskiy@novatc-gmbh.de>
2020-03-25 11:51:12 +0100

    fixed buildandrun.sh & Dockerfile. Application is now accessiable from localhost

--- micronaut-camunda-bpm-example/docker/native/buildandrun.sh
+++ micronaut-camunda-bpm-example/docker/native/buildandrun.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd ../../
-docker build -f docker/native/Dockerfile . -t micronaut && docker run -p 8080:8090 micronaut
+docker build -f docker/native/Dockerfile . -t micronaut
+docker run -p 8080:8080 micronaut
