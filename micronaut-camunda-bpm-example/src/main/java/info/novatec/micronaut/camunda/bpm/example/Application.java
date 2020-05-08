52e811caaf7a8db3a3e9135a9c86c061cd6984f7
Hayvon <aleksandrarshavskiy@gmail.com>
2020-05-08 16:54:53 +0200

    Edited Dockerfile

--- micronaut-camunda-bpm-example/src/main/java/info/novatec/micronaut/camunda/bpm/example/Application.java
+++ micronaut-camunda-bpm-example/src/main/java/info/novatec/micronaut/camunda/bpm/example/Application.java
@@ -5,6 +5,7 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("Test1");
         Micronaut.run(Application.class);
     }
 }
